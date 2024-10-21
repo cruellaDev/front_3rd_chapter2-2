4e62a64ea9c1a9f4d65901ca3a2b749db5e5936c
cruellaDev <cruellaDevLee@gmail.com>
2024-10-21 22:28:36 +0900

    * feat: cartUtils 기능 구현

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,58 @@
-import { CartItem, Coupon } from "../../../types";
+import { CartItem, Coupon } from '../../../types';
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  return (
+    item.product.price * item.quantity * (1 - getMaxApplicableDiscount(item))
+  );
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  return item.product.discounts.reduce(
+    (max, discount) =>
+      discount.quantity <= item.quantity ? Math.max(max, discount.rate) : max,
+    0
+  );
 };
 
-export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+export const calculateCartTotal = (
+  cart: CartItem[],
+  selectedCoupon: Coupon | null
+) => {
+  const totalBeforeDiscount = cart.reduce(
+    (sum, item) => sum + item.quantity * item.product.price,
+    0
+  );
+  const totalDiscountByItem = cart.reduce(
+    (sum, item) =>
+      sum + item.product.price * item.quantity * getMaxApplicableDiscount(item),
+    0
+  );
+  const totalDiscountByCoupon = !selectedCoupon
+    ? 0
+    : selectedCoupon.discountType === 'amount'
+    ? selectedCoupon.discountValue
+    : ((totalBeforeDiscount - totalDiscountByItem) *
+        selectedCoupon.discountValue) /
+      100;
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount:
+      totalBeforeDiscount - totalDiscountByItem - totalDiscountByCoupon,
+    totalDiscount: totalDiscountByItem + totalDiscountByCoupon,
   };
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+export const updateCartItemQuantity = (
+  cart: CartItem[],
+  productId: string,
+  newQuantity: number
+): CartItem[] => {
+  if (newQuantity === 0) {
+    return cart.filter((item) => item.product.id !== productId);
+  }
+  return cart.map((item) =>
+    item.product.id === productId
+      ? { ...item, quantity: Math.min(item.product.stock, newQuantity) }
+      : item
+  );
 };
