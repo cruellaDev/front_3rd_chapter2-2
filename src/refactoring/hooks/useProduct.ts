17d77290fba81385536ae71d0b21f6855073e741
cruellaDev <cruellaDevLee@gmail.com>
2024-10-21 22:28:53 +0900

    * feat: useProduct 기능 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,5 +2,25 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (product: Product) => {
+    const currentProduct = products.find((item) => item.id === product.id);
+    if (!currentProduct) return;
+    setProducts((prevProducts) =>
+      prevProducts.map((item) =>
+        item.id === product.id ? { ...item, ...product } : item
+      )
+    );
+  };
+
+  const addProduct = (product: Product) => {
+    const newProduct = {
+      ...product,
+      id: product.id ?? `p${products.length + 1}`,
+    };
+    setProducts((prevProducts) => prevProducts.concat(newProduct));
+  };
+
+  return { products, updateProduct, addProduct };
 };
