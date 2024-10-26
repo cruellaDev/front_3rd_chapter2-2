7d24fd78cdde30d07e96fbbec6f028b09e9e6f5e
cruellaDev <cruellaDevLee@gmail.com>
2024-10-26 15:03:16 +0900

    * fix: index.ts 참조 수정

--- src/refactoring/hooks/index.ts
+++ src/refactoring/hooks/index.ts
@@ -1,7 +1,6 @@
 export * from './useCart.ts';
 export * from './useCoupon.ts';
 export * from './useProduct.ts';
-export * from './useAdminLayout.ts';
 export * from './useProductContext.ts';
 export * from './useCartContext.ts';
 export * from './useCouponContext.ts';
